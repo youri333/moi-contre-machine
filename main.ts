850026f32f3b2abffdf5adde7df47b5ddffa3e98
youri333 <83732831+youri333@users.noreply.github.com>
2021-05-06 19:27:16 -0400

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,12 @@
 let elo = 0
 let y = 0
 input.onButtonPressed(Button.A, function () {
-    led.plot(elo, y)
-    elo += 1
-    if (elo == 5) {
-        y += 1
-        elo = 0
-    }
-})
-input.onButtonPressed(Button.B, function () {
     while (true) {
-    	
+        led.plot(elo, y)
+        elo += 1
+        if (elo == 5) {
+            y += 1
+            elo = 0
+        }
     }
 })
